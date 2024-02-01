742cff9b87a1751bfc720eafda2fe5beb603bd5b
AlexTanui <tanuialex417@gmail.com>
2024-02-01 11:29:01 +0300

    added backend

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,3 +1,4 @@
+// Login.js
 import { useState } from "react";
 import { UserAuth } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
@@ -6,7 +7,7 @@ import Footer from "../components/Footer/Footer";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { user, logIn } = UserAuth();
+  const { logIn } = UserAuth();
   const navigate = useNavigate();
   const [error, setError] = useState("");
 
@@ -20,12 +21,30 @@ function Login() {
     e.preventDefault();
     setError("");
     try {
-      await logIn(email, password);
-      navigate("/#home");
-      goTop();
+      // Make a POST request to the Flask backend
+      const response = await fetch("http://localhost:5000/members/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+       body: JSON.stringify({ email, password }),
+
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // Log the response from the backend
+
+        // After successful login, you may want to handle it accordingly
+        navigate("/#home");
+        goTop();
+      } else {
+        const errorData = await response.json();
+        console.log(errorData); // Log the error message from the backend
+        setError(errorData.message);
+      }
     } catch (error) {
-      console.log(error);
-      setError(error.message);
+      console.error("Error during login:", error);
     }
   };
 
@@ -34,7 +53,7 @@ function Login() {
       <section className="login-section">
         <div className="login-banner relative justify-center flex">
           <h1 className="text-white absolute bottom-[25px] text-[3rem] font-bold">
-            Sign In 
+            Sign In
           </h1>
         </div>
         {/* form  */}
@@ -44,25 +63,25 @@ function Login() {
             className="flex flex-col py-40 px-20 bg-black w-[55rem] min450:w-full  shadow-xl"
           >
             {error ? (
-              <p className="text-white bg-[#007FFF] font-bold text-[1.6rem] px-10 py-5 text-center mb-10">
-                Wrong email or password
+              <p className="text-white bg-[#ff0336] font-bold text-[1.6rem] px-10 py-5 text-center mb-10">
+                {error}
               </p>
             ) : null}
             <label className="text-[2rem] text-white mb-3 font-medium ">
               Email
             </label>
             <input
-              className="text-[1.7rem] px-8 py-4 mb-10 w-full outline-[#007FFF] "
-              placeholder="Hercules Fitness@gymail.com"
+              className="text-[1.7rem] px-8 py-4 mb-10 w-full outline-[#ff0336] "
+              placeholder="gymate@gymail.com"
               type="email"
               onChange={(e) => setEmail(e.target.value)}
             ></input>
 
-            <label className="text-[2rem] text-white mb-3 font-medium outline-[#007FFF] outline-2">
+            <label className="text-[2rem] text-white mb-3 font-medium outline-[#ff0336] outline-2">
               Password
             </label>
             <input
-              className="text-[1.7rem] px-8 py-4 mb-10 w-full outline-[#007FFF] "
+              className="text-[1.7rem] px-8 py-4 mb-10 w-full outline-[#ff0336] "
               placeholder="password"
               type="password"
               onChange={(e) => setPassword(e.target.value)}
@@ -70,22 +89,22 @@ function Login() {
 
             <button
               type="submit"
-              className="bg-[#007FFF] text-white py-4 font-medium text-[2rem] w-full mt-10"
+              className="bg-[#ff0336] text-white py-4 font-medium text-[2rem] w-full mt-10"
             >
               Sign In
             </button>
             <div className="flex gap-4 items-center mt-16 min450:flex-col">
-              <p className="text-white text-[1.5rem]">New to Hercules Fitness?</p>
+              <p className="text-white text-[1.5rem]">New to Gymate?</p>
               <Link
                 to="/signup"
-                className="text-[#007FFF] font-bold text-[1.5rem]"
+                className="text-[#ff0336] font-bold text-[1.5rem]"
               >
                 Sign Up
               </Link>
             </div>
             <p className="text-[#ffffffbc] text-[1.4rem] mt-3">
-              <span className="text-[#007FFF]">Test Account</span> -
-              Hercules Fitness@gymail.com <span className="text-[#007FFF]"> / </span>
+              <span className="text-[#ff0336]">Test Account</span> -
+              gymate@gymail.com <span className="text-[#ff0336]"> / </span>
               testpassword123
             </p>
           </form>
